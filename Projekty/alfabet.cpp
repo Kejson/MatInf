764d84b4ce9ffec316d9c918c796abf74ce52263
Kejson <54462478+Kejson@users.noreply.github.com>
2020-10-03 15:24:17 +0200

    Create alfabet.cpp

---
+++ Projekty/alfabet.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+
+    char zna;
+    char zna1;
+
+    for(int i = 97; i<=122; i++){
+        zna = i;
+        cout<<zna<<endl;
+    }
+    for(int j = 65; j<=90; j++){
+        zna1 = j;
+        cout<<zna1<<endl;
+    }
+
+return 0;
+}
